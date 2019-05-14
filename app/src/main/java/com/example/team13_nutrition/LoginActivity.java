48e977c053cf07c034ad03fb1ae6c9809c8303a3
DrazorV <vagelisp.97@gmail.com>
2019-05-14 13:52:46 +0300

    SignUp Screen

--- app/src/main/java/com/example/team13_nutrition/LoginActivity.java
+++ app/src/main/java/com/example/team13_nutrition/LoginActivity.java
@@ -1,52 +1,31 @@
 package com.example.team13_nutrition;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView signUp;
+    ImageView next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-    }
+        signUp = findViewById(R.id.sign_up);
+        //next = findViewById(R.id.next);
 
-
-
-    public void perform_action(View view) {
-        TextView signup = findViewById(R.id.sign_up);
-
-
-        signup.setText("This text view is clicked");
-
-        signup.setTextColor(Color.GREEN);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+//        next.setOnClickListener(v -> {
+//            Intent it = new Intent(LoginActivity.this, LoginActivity.class);
+//            startActivity(it);
+//
+//        });
+        signUp.setOnClickListener(v -> {
+            Intent it = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(it);
+        });
     }
 }
---
+++ app/src/main/java/com/example/team13_nutrition/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.team13_nutrition;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(v -> {
+            Intent it= new Intent(SignUpActivity.this,LoginActivity.class);
+            startActivity(it);
+        });
+    }
+}
