8f86c916f2b769841b7b959ecab8448ef20a72b7
DrazorV <vagelisp.97@gmail.com>
2019-05-14 14:38:09 +0300

    Screens fixed

--- app/src/main/java/com/example/team13_nutrition/SignUpActivity.java
+++ app/src/main/java/com/example/team13_nutrition/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.team13_nutrition;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -13,11 +12,11 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
 
-        back = findViewById(R.id.back);
+        //back = findViewById(R.id.back);
 
-        back.setOnClickListener(v -> {
-            Intent it= new Intent(SignUpActivity.this,LoginActivity.class);
-            startActivity(it);
-        });
+//        back.setOnClickListener(v -> {
+//            Intent it= new Intent(SignUpActivity.this,LoginActivity.class);
+//            startActivity(it);
+//        });
     }
 }
