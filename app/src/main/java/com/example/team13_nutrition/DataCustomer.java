56d74c2a7b2506f371ee9a74bd620e7b96900787
ManosMorf22 <manosmorf97@gmail.com>
2019-05-14 19:14:51 +0300

    DataCustomer in order to control customer

--- app/src/main/java/com/example/team13_nutrition/DataCustomer.java
+++ app/src/main/java/com/example/team13_nutrition/DataCustomer.java
@@ -3,6 +3,6 @@ package com.example.team13_nutrition;
 public class DataCustomer {
     private static Customer c;
     public static  Customer getCustomer(){
-        return c;
+        return c;//
     }
 }
