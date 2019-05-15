1fdef7015a918c2750401546c514b44cc41aa09f
DrazorV <vagelisp.97@gmail.com>
2019-05-15 19:22:02 +0300

    Exceptions

---
+++ app/src/main/java/com/example/team13_nutrition/exceptions/HeightException.java
@@ -0,0 +1,8 @@
+package com.example.team13_nutrition.exceptions;
+public class HeightException extends Exception {
+
+    public String toString(){
+        return"valid value is betwwen 0-7";
+
+    }
+}
