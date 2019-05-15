450c419169ee822d1e7a3fc25a6a91c28cf6b38b
DrazorV <vagelisp.97@gmail.com>
2019-05-15 13:08:29 +0300

    Added Screen

--- app/src/main/java/com/example/team13_nutrition/DataCustomer.java
+++
@@ -1,8 +0,0 @@
-package com.example.team13_nutrition;
-
-public class DataCustomer {
-    private static Customer c;
-    public static  Customer getCustomer(){
-        return c;//
-    }
-}
--- app/src/main/java/com/example/team13_nutrition/LoginActivity.java
+++ app/src/main/java/com/example/team13_nutrition/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.example.team13_nutrition;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     TextView signUp;
     ImageView login;
-
+    EditText username;
+    EditText password;
+    Map<String,Customer> customerMap = new HashMap<>();
 
 
     @Override
@@ -18,16 +28,37 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         signUp = findViewById(R.id.sign_up);
         login = findViewById(R.id.login);
-
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
         login.setOnClickListener(v -> {
-            Intent it = new Intent(LoginActivity.this, LoginActivity.class);
-            startActivity(it);
-
+            hideKeyboard(this);
+            if(customerMap.keySet().contains(username.getText().toString())){
+                if(customerMap.get(username.getText().toString()).getPassword().equals(password.getText().toString())){
+                    Intent it = new Intent(LoginActivity.this, LoginActivity.class);
+                    startActivity(it);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Password!", Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(getApplicationContext(), "This Username doesn't exist please sign up below!", Toast.LENGTH_SHORT).show();
+            }
         });
 
         signUp.setOnClickListener(v -> {
+            hideKeyboard(this);
             Intent it = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(it);
         });
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
