8cfdba9da16b98e1b0780bb613eb43c9492c1528
ManosMorf22 <manosmorf97@gmail.com>
2019-05-16 14:23:24 +0300

    SuggestedSports Activity Presenter

---
+++ app/src/main/java/com/example/team13_nutrition/SuggestedSportsPresenter.java
@@ -0,0 +1,32 @@
+package com.example.team13_nutrition;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SuggestedSportsPresenter {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private SuggestedSportsActivity view;
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public SuggestedSportsPresenter(SuggestedSportsActivity view){
+        this.view=view;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public ArrayList<Excercise> createList(){
+        Map<String,Customer> c=CustomerMap.customerMap;
+        Map.Entry<String,Customer> entry=c.entrySet().iterator().next();
+        String key=entry.getKey();
+        Customer customer=entry.getValue();
+        try {
+            HashMap<String,Object> fs=customer.calculateFoodsAndExcercises();
+            ArrayList<Excercise> excercises= (ArrayList<Excercise>) fs.get(1);
+            return  excercises;
+        }catch (Exception e){
+            return null;
+        }
+
+    }
+}
