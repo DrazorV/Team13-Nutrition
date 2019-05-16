65a863a7cbf89a4eaf962737b4b5b5e52fca9cb4
DrazorV <vagelisp.97@gmail.com>
2019-05-16 14:59:58 +0300

    Added Tabs and Better LogIn Screen

--- app/src/main/java/com/example/team13_nutrition/SuggestedSportsPresenter.java
+++ app/src/main/java/com/example/team13_nutrition/SuggestedSportsPresenter.java
@@ -1,23 +1,22 @@
 package com.example.team13_nutrition;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;//
+import java.util.Map;
 
 class SuggestedSportsPresenter {
 
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     ArrayList<Excercise> createList() {
         Map<String, Customer> c = CustomerMap.customerMap;
         Map.Entry<String, Customer> entry = c.entrySet().iterator().next();
         String key = entry.getKey();
         Customer customer = entry.getValue();
         try {
-            HashMap fs = customer.calculateFoodsAndExcercises();
+            HashMap fs = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                fs = customer.calculateFoodsAndExcercises();
+            }
             return (ArrayList<Excercise>) fs.get(1);
         } catch (Exception e) {
             return null;
