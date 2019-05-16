4705949905a6679c89a3f80a9aeffcba28b875ff
DrazorV <vagelisp.97@gmail.com>
2019-05-16 19:10:20 +0300

    Added Tabs and Better LogIn Screen

--- app/src/main/java/com/example/team13_nutrition/ui/main/Tab2.java
+++ app/src/main/java/com/example/team13_nutrition/ui/main/Tab2.java
@@ -1,16 +1,14 @@
 package com.example.team13_nutrition.ui.main;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.team13_nutrition.CustomerMap;
@@ -37,27 +35,27 @@ public class Tab2 extends Fragment {
         DecimalFormat df = new DecimalFormat("#.##");
         Nutrition_Goal temp = null;
         String user = Objects.requireNonNull(getArguments()).getString("params");
-        
-        TextView name = Objects.requireNonNull(getView()).findViewById(R.id.name);
+
+        EditText name = Objects.requireNonNull(getView()).findViewById(R.id.name);
         String nm = Objects.requireNonNull(CustomerMap.customerMap.get(user)).getName();
 
-        TextView age = Objects.requireNonNull(getView()).findViewById(R.id.age);
+        EditText age = Objects.requireNonNull(getView()).findViewById(R.id.age);
         int ag = Objects.requireNonNull(CustomerMap.customerMap.get(user)).getAge();
 
-        TextView weight = Objects.requireNonNull(getView().findViewById(R.id.weight));
+        EditText weight = Objects.requireNonNull(getView().findViewById(R.id.weight));
         double wght = Objects.requireNonNull(CustomerMap.customerMap.get(user)).getWeight();
 
-        TextView height = Objects.requireNonNull(getView().findViewById(R.id.height));
+        EditText height = Objects.requireNonNull(getView().findViewById(R.id.height));
         double hght = Objects.requireNonNull(CustomerMap.customerMap.get(user)).getHeight();
 
-        TextView gender = Objects.requireNonNull(getView().findViewById(R.id.gender));
+        EditText gender = Objects.requireNonNull(getView().findViewById(R.id.gender));
         String gndr = Objects.requireNonNull(CustomerMap.customerMap.get(user)).getGender();
 
 
         TextView bmi = Objects.requireNonNull(getView().findViewById(R.id.bmi));
         String bm = df.format(Objects.requireNonNull(CustomerMap.customerMap.get(user)).BMI(wght));
 
-        TextView target = Objects.requireNonNull(getView().findViewById(R.id.target));
+        EditText target = Objects.requireNonNull(getView().findViewById(R.id.target));
         Set<Nutrition_Goal> set = Objects.requireNonNull(CustomerMap.customerMap.get(user)).getNutriton_goals();
         for (Nutrition_Goal value: set) if(value.isActive()) temp = value;
         String trgt;
@@ -67,8 +65,6 @@ public class Tab2 extends Fragment {
             trgt = ("Lose " + (wght - Objects.requireNonNull(temp).getTargetWeight()) + " kg");
         else trgt = "Maintain Weight";
 
-        ImageView edit = getView().findViewById(R.id.edit);
-
         name.setText(nm +" "+ Objects.requireNonNull(CustomerMap.customerMap.get(user)).getSurname());
         age.setText(ag + " years old");
         weight.setText(wght + " kg");
@@ -77,23 +73,8 @@ public class Tab2 extends Fragment {
         bmi.setText(bm);
         target.setText(trgt);
 
+        name.setOnClickListener(v -> {
 
-        edit.setOnClickListener(v -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            builder.setTitle("Title");
-
-            // Set up the input
-            final TextView input = name;
-            // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-            input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-            builder.setView(input);
-
-            // Set up the buttons
-            builder.setPositiveButton("OK", (dialog, which) -> m_Text = input.getText().toString());
-            builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-
-            builder.show();
         });
     }
-    private String m_Text = "";
 }
